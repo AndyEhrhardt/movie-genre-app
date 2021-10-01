df4c360f301cf43e1c943c1d86e1476a3f886adc
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-10-01 16:05:12 -0500

    added basic pop up no movie info in it yet

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 import MovieDetails from '../MovieDetails/MovieDetails'
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
+import {HashRouter as Router, Route} from 'react-router-dom';
 
 function MovieList() {
 
@@ -27,7 +31,13 @@ function MovieList() {
                         <Route path="/moviedetails" >
                             <MovieDetails />
                         </Route>
-
+                        <Popup
+                            trigger={<button className="button"> Open Modal </button>}
+                            modal
+                            nested
+                        >
+                            <MovieDetails />
+                        </Popup>
                         </>
                     );
                 })}
